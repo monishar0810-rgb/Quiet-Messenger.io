86ed8ff184088420e10c24c58062ca8852db22bf
monishar0810-rgb <monisha.r0810@gmail.com>
2026-07-19 16:45:17 +0530

    Add files via upload

--- main.tsx
+++ main.tsx
@@ -1,10 +1,18 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=7b79e978"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=7b79e978"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=7b79e978"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+import "/src/index.css";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "/app/applet/src/main.tsx?__aistudio_auth_token=2qd0lWQ1gE5GmZhAQXX-kfqcFSf_x63p-XEQ5OEX9go%3D",
+    lineNumber: 8,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "/app/applet/src/main.tsx?__aistudio_auth_token=2qd0lWQ1gE5GmZhAQXX-kfqcFSf_x63p-XEQ5OEX9go%3D",
+    lineNumber: 7,
+    columnNumber: 3
+  }, this)
 );
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHN4P19fYWlzdHVkaW9fYXV0aF90b2tlbj0ycWQwbFdRMWdFNUdtWmhBUVhYLWtmcWNGU2ZfeDYzcC1YRVE1T0VYOWdvJTNEIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7U3RyaWN0TW9kZX0gZnJvbSAncmVhY3QnO1xuaW1wb3J0IHtjcmVhdGVSb290fSBmcm9tICdyZWFjdC1kb20vY2xpZW50JztcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAudHN4JztcbmltcG9ydCAnLi9pbmRleC5jc3MnO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykhKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9TdHJpY3RNb2RlPixcbik7XG4iXSwibWFwcGluZ3MiOiJBQU9JO0FBUEosU0FBUSxrQkFBaUI7QUFDekIsU0FBUSxrQkFBaUI7QUFDekIsT0FBTyxTQUFTO0FBQ2hCLE9BQU87QUFFUCxXQUFXLFNBQVMsZUFBZSxNQUFNLENBQUUsRUFBRTtBQUFBLEVBQzNDLHVCQUFDLGNBQ0MsaUNBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBQUssS0FEUDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBRUE7QUFDRjsiLCJuYW1lcyI6W119
